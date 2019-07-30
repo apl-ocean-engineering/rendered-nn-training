5e92eb1d5568fe295e8dd917aab08ea8418c91f0
cgobat <cgobat@uw.edu>
2019-07-29 18:07:50 -0700

    Wipe instances after each frame grab

--- instancing.py
+++ instancing.py
@@ -4,18 +4,12 @@ from direct.gui.OnscreenImage import OnscreenImage
 from direct.gui.OnscreenText import OnscreenText
 from panda3d.core import *
 
-def renderToPNM():
-    base.graphicsEngine.renderFrame() # Render the frame
-
-    image = PNMImage() # init variable to store the image (PNMImage is an image manipulation class native to Panda3D)
-    dr = base.camNode.getDisplayRegion(0) # set display region to the default
-    dr.getScreenshot(image) # grab a screenshot of the DR and write it to the image
-
-    return image
+def coordToImagespace(coord):
+    x = (coord[0]+1)/2
+    y = (((-1)*coord[2])+1)/2
+    return LPoint2f(x,y)
 
 mine = loader.loadModel("mine.egg") 
-mineParent = render.attachNewNode('mineParent')
-
 light = Spotlight("slight")
 
 camLens = base.cam.node().getLens()
@@ -31,13 +25,7 @@ props = WindowProperties()
 props.setSize(w, h) 
 base.win.requestProperties(props)
 
-def coordToImagespace(coord):
-    x = (coord[0]+1)/2
-    y = 0
-    z = (((-1)*coord[2])+1)/2
-    return LPoint3f(x,y,z)
-
-for i in range(5):
+def rerender(task):
     scene_id = random.randint(0,354)
     #background = OnscreenImage(parent = render2d, image = "/home/caden/Pictures/backgrounds/bg_{}.png".format(scene_id)) # load background image
     base.cam2d.node().getDisplayRegion(0).setSort(-1) # make sure it renders behind everything else
@@ -48,7 +36,10 @@ for i in range(5):
     proj_mat = camLens.get_projection_mat_inv() # read the lens' inverse projection matrix
 
     num_mines = 3#int(random.triangular(1,3,1))
-    labelFile = open("/home/caden/Pictures/mines2/labels/scene_{}.txt".format(i), "w+") # create the label file 
+    count = task.frame
+    labelFile = open("/home/caden/Pictures/mines2/labels/scene_{}.txt".format(count), "w+") # create the label file 
+
+    mineParent = render.attachNewNode('mineParent')
 
     for n in range(num_mines):
         minePlacer = mineParent.attachNewNode("minePlacer")
@@ -81,13 +72,21 @@ for i in range(5):
         spot.lookAt(minePlacer) # point it at the mine, wherever it is
         minePlacer.setLight(spot) # assign the light to the mine
 
-        print >>labelFile, str(0)+" "+str(coordToImagespace(center)[0])+" "+str(coordToImagespace(center)[2])+" "+str(box_w/2)+" "+str(box_h/2)
+        print >>labelFile, str(0)+" "+str(coordToImagespace(center)[0])+" "+str(coordToImagespace(center)[1])+" "+str(box_w/2)+" "+str(box_h/2)
 
     image = PNMImage() # create PNMImage wrapper
     base.camNode.getDisplayRegion(0).getScreenshot(image) # grab a PNM screenshot of the display region
-    imageFile = "/home/caden/Pictures/mines2/images/scene_{}.jpg".format(i) # set the filename (don't know why images and labels are 1 offset, but they are)
+    imageFile = "/home/caden/Pictures/mines2/images/scene_{}.jpg".format(count-1) # set the filename (don't know why images and labels are 1 offset, but they are)
     image.write(Filename(imageFile)) # write the screenshot to the above file
-    #labelFile.write(str(0)+" "+str(coordToImagespace(center)[0])+" "+str(coordToImagespace(center)[2])+" "+str(box_w/2)+" "+str(box_h/2)+"\n")
     labelFile.close()
+    
+    mineParent.removeNode()
 
+    if count < 10:
+        return task.cont
+    else:
+        print "Render complete."
+        return task.done
+
+base.taskMgr.add(rerender, "render")
 base.run()
