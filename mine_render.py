3afaf10b08ed8f5b5d390da272609fc4e1b5ef31
cgobat <cgobat@uw.edu>
2019-08-06 14:28:34 -0700

    Original file; updates for compatibility

--- mine_render.py
+++ mine_render.py
@@ -4,7 +4,7 @@ from direct.gui.OnscreenImage import OnscreenImage
 from direct.gui.OnscreenText import OnscreenText
 from panda3d.core import *
 
-mine = loader.loadModel("/home/caden/.local/lib/python2.7/site-packages/panda3d/models/mine.egg") # load model
+mine = loader.loadModel("mine.egg") # load model
 mine.reparentTo(render) # add model to scene
 
 light = Spotlight("slight")
@@ -48,10 +48,10 @@ def coordToImagespace(coord):
     z = (((-1)*coord[2])+1)/2
     return LPoint3f(x,y,z)
 
-for i in range(4):
+for i in range(10):
     scene_id = random.randint(0,354)
-    #background = OnscreenImage(parent = render2d, image = "/home/caden/Pictures/backgrounds/bg_{}.png".format(scene_id)) # load background image
-    base.cam2d.node().getDisplayRegion(0).setSort(-1) # make sure it renders behind everything else
+    background = OnscreenImage(parent = render2d, image = "/home/caden/Pictures/backgrounds/bg_{}.png".format(scene_id)) # load background image
+    base.cam2d.node().getDisplayRegion(0).setSort(-20) # make sure it renders behind everything else
     
     mine_x = random.uniform(-3.5,3.5)
     mine_y = random.uniform(5,10)
@@ -70,16 +70,13 @@ for i in range(4):
     mine.setPos(mine_x,mine_y,mine_z) # set random position
     mine.setHpr(mine_H,mine_p,mine_r) # set random orientation
     mine.setScale(mine_scale) # set random size
-    projectedPos = compute2dPosition(mine, (0,0,0))
-    #xCenter = (projectedPos[0]+1.0)/2.0
-    #yCenter = ((-1.0)*projectedPos[1]+1.0)/2.0
 
     light.setColor((light_R/255, light_G/255, light_B/255, 1)) # set light color and intensity
     spot = render.attachNewNode(light)
     spot.setPos(light_x,light_y,light_z) # set random position
     spot.lookAt(mine) # point it at the mine, wherever it is
     mine.setLight(spot) # use the spot as the lighting for the rendered scene
-
+    
     proj_dummy = base.cam.attach_new_node("proj-dummy") # create a new node to hold the projected model
     line_node = GeomNode("lines")
     line_path = render2d.attach_new_node(line_node)
@@ -94,9 +91,8 @@ for i in range(4):
     box_w = (max[0] - min[0])
     box_h = (max[1] - min[1])
     center = LPoint3f(min[0]+box_w/2,0,min[1]+(box_h/2))
-    print diagonal, center
 
-    segs = LineSegs()
+    '''segs = LineSegs()
     segs.move_to(box_LL)
     segs.draw_to(min[0], 0, max[1])
     segs.draw_to(box_UR)
@@ -107,13 +103,12 @@ for i in range(4):
     segs.draw_to(center)
 
     line_node.remove_all_geoms()
-    segs.create(line_node)
+    segs.create(line_node)'''
 
-    imageFile = "/home/caden/Pictures/replacements/images/scene_{}.jpg".format(i)
-    renderToPNM().write(Filename(imageFile))
-    print("generated "+imageFile)
-    labelFile = open("/home/caden/Pictures/replacements/labels/scene_{}.txt".format(i), "w+")
-    labelFile.write(str(0)+" "+str(coordToImagespace(center)[0])+" "+str(coordToImagespace(center)[2])+" "+str(box_w/2)+" "+str(box_h/2))
+    labelFile = open("/home/caden/Pictures/mines2/labels/scene_{}.txt".format(i), "w+") # create the label file 
+    base.graphicsEngine.renderFrame() # Render the frame
+    image = PNMImage() # create PNMImage wrapper
+    base.camNode.getDisplayRegion(0).getScreenshot(image) # grab a PNM screenshot of the display region
+    imageFile = Filename("/home/caden/Pictures/mines2/images/scene_{}.jpg".format(i)) # set the filename (don't know why images and labels are 1 offset, but they are)
+    image.write(imageFile) # write the screenshot to the above file
     labelFile.close()
-
-    base.run()
